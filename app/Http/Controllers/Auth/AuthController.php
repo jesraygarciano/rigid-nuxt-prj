cf43ad2c8b0e35645a436297f9608b288a12750b
Jesray Garciano <jesray.garciano@gmail.com>
2019-01-23 10:04:22 +0800

    Added validation to register and save it to database

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -8,4 +8,22 @@ use App\Http\Controllers\Controller;
 class AuthController extends Controller
 {
     //
+
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|unique:users,email|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return $user;
+        
+    }
 }
