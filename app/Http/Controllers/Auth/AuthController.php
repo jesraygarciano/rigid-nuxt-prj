efed0406833f9f33e57acf2230c0eb6b44750ccd
Jesray Garciano <jesray.garciano@gmail.com>
2019-01-23 10:13:06 +0800

    Add Auth Controller and created a resource

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 
 class AuthController extends Controller
 {
@@ -23,7 +25,7 @@ class AuthController extends Controller
             'password' => bcrypt($request->password)
         ]);
 
-        return $user;
-        
+        return new UserResource($user);
+
     }
 }
